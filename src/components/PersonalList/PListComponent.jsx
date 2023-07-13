7ae53f828be49aff8b11c397f119ee0778497c17
austinbhan <austinbhan@outlook.com>
2023-07-13 10:07:28 -0700

    Relocated edit and delete pages

--- src/components/PersonalList/PListComponent.jsx
+++ src/components/PersonalList/PListComponent.jsx
@@ -1,14 +1,37 @@
+import { useState } from 'react';
+import { deleteBook } from '../../services/fetch-utils';
+
 export default function PersonaListComponent({ 
+  id,
   bookTitle, 
   bookAuthor, 
   bookYear }) {
+
+  const [remove, setRemove] = useState('');
+
+  async function handleDelete(e) {
+    e.preventDefault();
+    await deleteBook(remove);
+    window.location.replace('/listPage');
+  }
+
+  function handleEditLink() {
+    window.location.replace(`/editPage/${id}`);
+  }
+
   return (
     <div>
-      <ul>
-        <li>{bookTitle}</li>
-        <li>{bookAuthor}</li>
-        <li>{bookYear}</li>
-      </ul>
+      <form onSubmit={handleDelete}>
+        <ul>
+          <li>{bookTitle}</li>
+          <li>{bookAuthor}</li>
+          <li>{bookYear}</li>
+        </ul>
+        <button value={id} 
+          onClick={e => setRemove(e.target.value)}>
+            Delete {bookTitle}</button>
+      </form>
+      <button onClick={handleEditLink}>Edit {bookTitle}</button>
     </div>
   );
 }
